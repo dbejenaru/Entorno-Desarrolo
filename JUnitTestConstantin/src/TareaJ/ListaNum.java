bdb0b7cfc282a85b010028dce6e90462c8cb1113
dbejenaru <bejenarudragos@gmail.com>
2018-05-28 17:10:20 +0200

    no message

--- JUnitTestConstantin/src/TareaJ/ListaNum.java
+++ JUnitTestConstantin/src/TareaJ/ListaNum.java
@@ -1,8 +1,24 @@
 package TareaJ;
+/**
+ * Clase ListaNum
+ *
+ * Muestra el numero mayor de un array de numeros
+ *
+ * @author Constantin Dragos
+ * @version 1.0
+ */
 
 public class ListaNum 
 {
-	
+	//Metodos publicos
+ 
+    /**
+     * Comprueba cual numero es mayor
+     * @param lista
+     * @return <ul>
+     *          <li>t numero mayor </li>
+     *          </ul>
+     */
 	public static int NumMayor(int lista[]) 
 	{
 
